7923908f0d62c7d513b8ba0f9895fdd27948915b
qq1065507891 <42400109+qq1065507891@users.noreply.github.com>
2018-11-21 22:25:19 +0800

    Update create_tfrecord.py

--- create_tfrecord.py
+++ create_tfrecord.py
@@ -7,13 +7,13 @@ from PIL import Image
 import numpy as np
 
 # 验证集数量
-_NUM_TEST = 500
+_NUM_TEST = 5000
 # 随机种子
 _RANDOM_SEED = 0
 # 数据集路径
-DATASET_DIR =  'D:/Tensorflow/captcha/iamges/'
+DATASET_DIR = r"F:\程序\tfrecoad\TensorFlow_test-master/images/"
 # tfrecord 文件存放路径
-TFRECORD_DIR =  'D:/Tensorflow/captcha/'
+TFRECORD_DIR = r"F:\程序\tfrecoad\TensorFlow_test-master/captcha/"
 
 # 判断tfrecord文件文件是否存在
 def _dataset_exists(dataset_dir):
@@ -48,10 +48,10 @@ def image_to_tfexample(image_data,label0,label1,label2,label3):
     # Abstract base class for protool meages.
     return tf.train.Example(features=tf.train.Features(feature={
         'image': bytes_feature(image_data),
-        'label0': int64_feature(label0),
-        'label1': int64_feature(label1),
-        'label2': int64_feature(label2),
-        'label3': int64_feature(label3),
+        'label0': bytes_feature(label0),
+        'label1': bytes_feature(label1),
+        'label2': bytes_feature(label2),
+        'label3': bytes_feature(label3),
     }))
 
 #  把数据转为TFRecord格式
@@ -64,13 +64,13 @@ def _convert_dataset(split_name, filenames, dataset_dir):
         with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
             for i, filename in enumerate(filenames):
                 try:
-                    sys.stdout.write('r>> Converting image %d/%d' % (i+1, len(filenames)))
+                    sys.stdout.write('\r>> Converting image %d/%d' % (i+1, len(filenames)))
                     sys.stdout.flush()
 
                     # 读取图片
                     image_data = Image.open(filename)
                     # 根据模型的结构resize
-                    image_data = image_data.resize((224,224))
+                    image_data = image_data.resize((28, 28))
                     # 灰度化
                     image_data = np.array(image_data.convert('L'))
                     # 将图片转为bytes
@@ -80,7 +80,7 @@ def _convert_dataset(split_name, filenames, dataset_dir):
                     labels = filename.split('/')[-1][0:4]
                     num_labels = []
                     for j in range(4):
-                        num_labels.append(int(labels[j]))
+                        num_labels.append(labels[j].encode())
 
                     # 生成protocol数据类型
                     example = image_to_tfexample(image_data, num_labels[0], num_labels[1], num_labels[2], num_labels[3])
@@ -93,20 +93,20 @@ def _convert_dataset(split_name, filenames, dataset_dir):
             sys.stdout.write('\n')
             sys.stdout.flush()
 
-        # 判断tfrecord文件是否存在
-        if _dataset_exists(TFRECORD_DIR):
-            print('tfceord文件已存在')
-        else:
-            # 获得所有图片
-            photo_filenames = _get_filename_and_classes(DATASET_DIR)
+# 判断tfrecord文件是否存在
+if _dataset_exists(TFRECORD_DIR):
+    print('tfceord文件已存在')
+else:
+    # 获得所有图片
+    photo_filenames = _get_filename_and_classes(DATASET_DIR)
 
-            # 把数据初分为训练集和测试集，并打乱
-            random.seed(_RANDOM_SEED)
-            random.shuffle(photo_filenames)
-            training_filenames = photo_filenames[_NUM_TEST:]
-            testing_filenames = photo_filenames[:_NUM_TEST]
+    # 把数据初分为训练集和测试集，并打乱
+    random.seed(_RANDOM_SEED)
+    random.shuffle(photo_filenames)
+    training_filenames = photo_filenames[_NUM_TEST:]
+    testing_filenames = photo_filenames[:_NUM_TEST]
 
-            # 数据转换
-            _convert_dataset('train', training_filenames, DATASET_DIR)
-            _convert_dataset('test', testing_filenames, DATASET_DIR)
-            print('生成tfcecord文件')
+    # 数据转换
+    _convert_dataset('train', training_filenames, DATASET_DIR)
+    _convert_dataset('test', testing_filenames, DATASET_DIR)
+    print('生成tfcecord文件')
